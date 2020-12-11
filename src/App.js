5c433ca2dc4a91ff044d7417ffd581c397b4b7e6
Bashar Boutros <47969145+BasharBoutros@users.noreply.github.com>
2020-12-11 16:08:35 +0100

    Bashar3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn python
+          Learn Bashar Raslan
         </a>
       </header>
     </div>
