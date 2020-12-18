6bff0eb9467119be81c61aa1d9ae95a5fdecdf71
raslan3333 <raslan3333@gmail.com>
2020-12-18 16:46:09 +0100

    first change1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+import ShoppingList from './components/ShoppingList.js';
+import NavBar from './components/NavBar.js';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Bashar Raslan
-        </a>
-      </header>
+        <NavBar/>
+    <ShoppingList/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+
+
+class NavBar extends Component {
+
+    render() {
+        const ul = {
+            alignItems:"center",
+
+            display: "flex",
+            justifyContent: "space-between",
+            listStyle: "none",
+            a: {
+                textDecoration: "none"
+            }
+        };
+        const a = {
+            textDecoration: "none"
+        }
+        const li = {
+            paddingRight: 20,
+            PaddingLeft:20,
+            inlineBlock: "none"
+        }
+        const menu = {
+            paddingLeft:20
+        }
+        return (
+            <div className="nav-bar" >
+                <header style={ul}>
+                    <p style={menu}>menu</p>
+                    <h1>Wake up sheeple!</h1>
+                    <nav>
+                        <ul style={ul}>
+
+                            <li style={li}><a href="articles.html" style={a}>Search</a></li>
+                            <li style={li}><a href="today.html">Login</a></li>
+                            <li style={li}><a href="successes.html">wishlist</a></li>
+                            <li style={li}><a href="successes.html">Cart</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        );
+    }
+}
+export default NavBar;
+
+
+
+
